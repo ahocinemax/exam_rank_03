23877eb988570f50fe005e5eb207e4c059f089e3
ahocine <ahocine@student.42.fr>
2022-09-05 01:28:46 +0200

    Adding ft_display_int/ft_putnbr

--- ft_printf/ft_printf.c
+++ ft_printf/ft_printf.c
@@ -3,40 +3,77 @@
 #include <stdlib.h>
 #include <stdarg.h>
 
-void	ft_putchar(char c, int , *count)
+int		ft_strlen(char *str)
+{
+	int i = 0;
+
+	if (!str)
+		return (0);
+	while (str[i])
+		i++;
+	return (i);
+}
+
+void	ft_putchar(char c, int *count)
 {
 	write(1, &c, 1);
-	(*count)++;
+	if (count)
+		(*count)++;
 }
 
 void    ft_putstr(char *str, int *count)
 {
+	int	i = 0;
 	if (!str)
 		return ;
-	while (str)
-		ft_putchar((*str)++, &count);
+	while (str[i])
+		ft_putchar(str[i++], count);
 }
 
-void	ft_display_int(va_list lst_param, &count)
+void	ft_putnbr(int nb, int *count)
+{
+	if (nb == -2147483648)
+		ft_putstr("-2147483648", count);
+	if (nb < 0)
+	{
+		nb *= -1;
+		ft_putchar('-', count);
+	}
+	if (nb < 10)
+		ft_putchar(nb + '0', count);
+	else
+	{
+		ft_putnbr(nb / 10, count);
+		ft_putnbr(nb % 10, count);
+	}
+}
+
+void	ft_display_int(va_list lst_param, int *count)
 {
 	int nb = va_arg(lst_param, int);
-	if (nb <)
-		str = "(null)";
-    ft_putstr(str, count);
+    ft_putnbr(nb, count);
 }
 
-void	ft_display_str(va_list lst_param, &count)
+void	ft_display_str(va_list lst_param, int *count)
 {
 	char *str = va_arg(lst_param, char *);
 	if (!str)
 		str = "(null)";
-    ft_putstr(str, count);
+	ft_putstr(str, count);
+}
+
+void	ft_display_hex(va_list lst_param, int *count)
+{
+	(void)lst_param;
+	(void)count;
 }
 
 int ft_printf(char *line, ...)
 {
 	int i = 0;
 	int count = 0;
+	va_list	lst_param;
+
 	va_start(lst_param, line);
 	while (line[i])
 	{
@@ -52,26 +89,19 @@ int ft_printf(char *line, ...)
 			else if (line[i] == '%')
 				ft_putchar('%', &count);
 			else
-				return (va_end(lst_param), ft_putstr("parsing error\n"), count);
-			i++;
+				return (va_end(lst_param), ft_putstr("parsing error\n", NULL), -1);
 		}
 		else
 			ft_putchar(line[i], &count);
 		i++;
 	}
-	va_end(lst_param);
-	return count;
+	return (va_end(lst_param), count);
 }
 
 int main(void)
 {
-	va_list lst_param;
-
-	char *str = "Bonjour";
-	ft_putstr("----------------- MY FUNCTION -----------------\n");
-	int ret1 = ft_printf("%s\n", str);
-	ft_putstr("\n----------------- PRINTF RTRN -----------------\n");
-	int ret2 = printf("%s\n", str);
-	ft_putstr("\n----------------- RETURNS VAL -----------------\n");
-	printf("my_return: %d\noffReturn: %d\n", ret1, ret2);
+	char *str = "HelloWorld";
+	ft_printf("%s\n", str);
+	// int ret2 = printf("%s\n", str);
+	// printf("my_return: %d\noffReturn: %d\n", ret1, ret2);
 }
