a750be28b405385e48512f095376e11277540f81
ahocine <ahocine@student.42.fr>
2022-09-09 17:52:37 +0200

    FINAL PUSH

--- get_next_line/get_next_line.c
+++ get_next_line/get_next_line.c
@@ -1,19 +1,32 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <fcntl.h>
+#include <unistd.h>
 
 char	*get_next_line(int fd)
 {
-	static char	buffer[BUFFER_SIZE];
-	char		*ret;
-	int			lu;
-
-	if (fd < 0)
-		return (NULL);
-	lu = 1;
-	while (lu == BUFFER_SIZE && *line)
-	{
-		lu = read(fd, buffer, BUFFER_SIZE);
-		if (!lu)
-			break ;
-	}
+	char *str=malloc(99999999), *copy = str;
+	while (read(fd, copy, 1) > 0 && *copy++ != '\n');
+	return (copy > str) ? (*copy = 0, str) : (free(str), NULL);
 }
+
+// int	main(void)
+// {
+// 	char	*line;
+// 	char	*path;
+// 	int		i = 0;
+// 	int		fd;
+
+// 	path = "test.txt";
+// 	fd = open(path, O_RDONLY);
+// 	if (fd < 0)
+// 		return (printf("Error: Open failed\n"));
+// 	line = get_next_line(fd);
+// 	while (line || i < 1)
+// 	{
+// 		printf("line %d : {%s}\n", i++ + 1, line);
+// 		free(line);
+// 		line = get_next_line(fd);
+// 	}
+// 	return (0);
+// }
